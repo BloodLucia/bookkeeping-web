07d0a7c1df1ecade25b83a1a57496da48f156150
Kalougata <111719756+kalougata@users.noreply.github.com>
2023-11-15 04:59:06 +0000

    [➕]创建app.scss, 限制页面大小

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,9 @@ import '@unocss/reset/tailwind.css'
 // custom vite plugin
 import 'virtual:svgsprites'
 
+// styles
+import './app.scss'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
