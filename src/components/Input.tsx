fe9e5327335b50c984fb9718a7b20f6bba4556ce
kalougata <kalougata@111.com>
2023-11-15 21:37:45 +0800

    [➕]自定义Input组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,55 @@
+import { ChangeEvent, ReactNode } from "react"
+
+type Props<T> = {
+    label?: string | ReactNode
+    placeholder?: string
+    value?: T
+    onChange?: (value: T) => void
+    error?: string
+    disableError?: boolean
+    className?: string
+} & (
+        | { type?: 'text' }
+        | { type: 'emoji' }
+        | { type: 'date' }
+        | { type: 'sms_code'; request: () => Promise<unknown> }
+        | { type: 'select'; options: { value: string; text: string }[] }
+    )
+export const Input = <T extends string>(props: Props<T>) => {
+    const { label, placeholder, value, onChange: _onChange, error, disableError, className } = props
+    const onChange = (e: string | ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const value = typeof e === 'string' ? e : e.target.value
+        _onChange?.(value as T)
+    }
+    const common = { value, onChange, placeholder }
+    const renderInput = () => {
+        switch (props.type) {
+            case undefined:
+            case 'text':
+                return <input j-input-text type='text' {...common} />
+            case 'emoji':
+                return <></>
+            case 'select':
+                return <select className="h-36px" {...common}>
+                    {props.options.map(option =>
+                        <option key={option.value} value={option.value}>{option.text}</option>)
+                    }
+                </select>
+            case 'sms_code':
+                return <></>
+            case 'date':
+                return <></>
+            default:
+                return null
+        }
+    }
+    return (
+        <>
+            <div flex flex-col gap-y-8px className={className}>
+                {label ? <span text-18px>{label}</span> : null}
+                {renderInput()}
+                {disableError ? null : <span text-red text-12px>{error || '　'}</span>}
+            </div>
+        </>
+    )
+}
