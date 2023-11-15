7c46bbc2d252360a3e4c69f1b5e4794be464f330
Kalougata <111719756+kalougata@users.noreply.github.com>
2023-11-15 04:27:34 +0000

    vite中使用unocss, 创建unocss配置文件

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router.tsx'
 
+// unocss
+import 'virtual:uno.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
