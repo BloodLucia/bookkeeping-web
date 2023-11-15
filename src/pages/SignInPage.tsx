da3c9655b6f87756b55d832ee57f9bc5b4e702d8
Kalougata <111719756+kalougata@users.noreply.github.com>
2023-11-15 06:49:48 +0000

    [+]自定义Loading组件

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,9 +1,11 @@
 import { Gradient } from "../components/Gradient"
+import { Loading } from "../components/Loading"
 
 export const SignInPage: React.FC = () => {
     return (
         <>
             <Gradient>
+                <Loading />
                 Hello
             </Gradient>
         </>
