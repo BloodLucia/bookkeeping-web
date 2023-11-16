280fb72d6ba300d712d37b625a3e7d1754984719
kalougata <kalougata@111.com>
2023-11-16 17:43:11 +0800

    []App.tsx作为次要的入口文件

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import vhCheck from "vh-check"
+import styled from 'styled-components'
+import { useLoadingStore } from "./stores/useLoadingStore"
+import { usePopup } from "./hooks/usePopup"
+import { Icon } from "./components/Icon"
+import { useEffect } from "react"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routes/router"
+
+// unocss
+import 'virtual:uno.css'
+
+// custom vite plugin
+import 'virtual:svgsprites'
+
+// styles
+import './app.scss'
+
+vhCheck()
+
+const Spin = styled(Icon)`
+  animation: spin 1s linear infinite;
+  @keyframes spin {
+    from { transform: rotate(0deg); }
+    to { transform: rotate(360deg); }
+  }
+`
+export const App:React.FC = () => {
+  const { visible } = useLoadingStore()
+  const { popup, hide, show } = usePopup({
+    children: <div p-16px>
+      <Spin className="w-32px h-32px" name="loading" />
+    </div>,
+    position: 'center'
+  })
+  useEffect(() => {
+    visible ? show() : hide()
+  }, [visible])
+  
+  return (
+    <>
+      <RouterProvider router={router} />
+      {popup}
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/router.tsx'
-
-// unocss
-import 'virtual:uno.css'
-
-// custom vite plugin
-import 'virtual:svgsprites'
-
-// styles
-import './app.scss'
+import { App } from './App.tsx'
 
 const div = document.getElementById('root') as HTMLElement
-
 const root = ReactDOM.createRoot(div)
+
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )
+
 export { div as rootDiv }
