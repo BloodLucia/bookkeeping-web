dbc18ec097c0e1f46181c774070b0404a19f3a2a
kalougata <kalougata@111.com>
2023-11-16 08:15:13 +0800

    [⚙️]vite配置跨域

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { svgsprites } from './vite_plugins/svgsprites'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/": {
+        target: "http://127.0.0.1:8888/api/v1",
+        changeOrigin: false
+      }
+    }
+  },
   plugins: [
     react(),
     UnoCSS(),
