d209c4dd62527b975cec56955395a27f4d9fa025
kalougata <kalougata@111.com>
2023-11-16 10:02:20 +0800

    [➕]创建useSignInStore, 登录页的状态

---
+++ src/stores/useSignInStore.ts
@@ -0,0 +1,44 @@
+import { create } from 'zustand'
+import { FormError } from "../utils/validator"
+
+type Data = {
+  email: string
+  code: string
+}
+
+interface SignIn {
+  data: Data
+  error: FormError<Data>
+  setData: (data: Partial<Data>) => void
+  setError: (error: Partial<FormError<Data>>) => void
+}
+export const useSignInStore = create<SignIn>((set, get) => (
+  {
+    data: {
+      // TODO: 删掉这个邮箱地址
+      email: 'fangyinghang@foxmail.com',
+      code: '123456'
+    },
+    error: {
+      email: [],
+      code: []
+    },
+    setData: (data: Partial<Data>) => {
+      set(state => ({
+        ...state,
+        data: {
+          ...state.data,
+          ...data
+        }
+      }))
+    },
+    setError: (error: Partial<FormError<Data>>) => {
+      set(state => ({
+        ...state,
+        error: {
+          ...error
+        }
+      }))
+    }
+  }
+))
