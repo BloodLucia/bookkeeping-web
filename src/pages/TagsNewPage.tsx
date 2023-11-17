a0bb75affed0512dfcf3784e8ceca6921532c24e
kalougata <kalougata@111.com>
2023-11-17 09:47:42 +0800

    [✔️]新建标签页面布局

---
+++ src/pages/TagsNewPage.tsx
@@ -0,0 +1,13 @@
+import { Gradient } from "../components/Gradient"
+import { TopNav } from "../components/TopNav"
+
+export const TagsNewPage:React.FC = () => {
+  return (
+    <>
+      <Gradient grow-0 shrink-0>
+        <TopNav title="新建标签" icon={<BackIcon />} />
+      </Gradient>
+      <TagForm type="create" />
+    </>
+  )
+}
