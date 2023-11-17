f3d1ad5a6c1e2f41179d6e1bc6f3487d999c58e2
kalougata <kalougata@111.com>
2023-11-17 09:48:24 +0800

    [➕]创建标签页面布局

--- src/pages/TagsNewPage.tsx
+++ src/pages/TagsNewPage.tsx
@@ -5,7 +5,7 @@ export const TagsNewPage:React.FC = () => {
   return (
     <>
       <Gradient grow-0 shrink-0>
-        <TopNav title="新建标签" icon={<BackIcon />} />
+        <TopNav title="新建标签" icon={<BackIcon/>} />
       </Gradient>
       <TagForm type="create" />
     </>
